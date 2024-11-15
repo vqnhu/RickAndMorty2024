a328d95c532fdd4451c7f274cf9fdcfd0109045a
Sattar <sattar_saud@hotmail.com>
2024-11-15 12:47:49 +0100

    added logic to handle image selection

--- app/src/main/java/com/example/rickandmortyeksamen2024/components/DeleteItem.kt
+++ app/src/main/java/com/example/rickandmortyeksamen2024/components/DeleteItem.kt
@@ -1,34 +1,42 @@
 package com.example.rickandmortyeksamen2024.components
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
-import androidx.compose.material.icons.outlined.Delete
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.example.rickandmortyeksamen2024.data.CreateCharacter
 
 @Composable
 fun DeleteCharacterItem(deleteCharacter: CreateCharacter, onDelete: (() -> Unit)? = null) {
-    Column {
-        Row (verticalAlignment = Alignment.CenterVertically) {
-            Text(deleteCharacter.name)
-            Text(deleteCharacter.species)
-            Text(deleteCharacter.status)
-            Text(deleteCharacter.image)
-            if (onDelete != null) {
-                IconButton(onClick = { onDelete() }) {
-                    Icon(imageVector = Icons.Filled.Delete, contentDescription = null)
-                }
+    Column(
+        modifier = Modifier.padding(16.dp)
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Column(modifier = Modifier.weight(1f)) {
+                Text(text = "Name: ${deleteCharacter.name}")
+                Text(text = "Species: ${deleteCharacter.species}")
+                Text(text = "Status: ${deleteCharacter.status}")
+                Text(text = "Image: ${deleteCharacter.image}")
             }
 
-
+            if (onDelete != null) {
+                IconButton(onClick = { onDelete() }) {
+                    Icon(
+                        imageVector = Icons.Filled.Delete,
+                        contentDescription = "Delete Character"
+                    )
+                }
+            }
         }
     }
-
 }
